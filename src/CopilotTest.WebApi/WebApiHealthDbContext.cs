1c55649b18e0a4287de255a77d2136021a984af2
pposps <115163792+pposps@users.noreply.github.com>
2026-03-16 12:35:10 +0100

    Remove schema configuration comment
    Removed comment about schema configuration.

--- src/CopilotTest.WebApi/WebApiHealthDbContext.cs
+++ src/CopilotTest.WebApi/WebApiHealthDbContext.cs
@@ -14,7 +14,6 @@ internal class WebApiHealthDbContext : DbContext
     {
         base.OnModelCreating(modelBuilder);
 
-        // Configure schema for this bounded context
         modelBuilder.HasDefaultSchema("Health");
 
         modelBuilder.Entity<Health>(entity =>
